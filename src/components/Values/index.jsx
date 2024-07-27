668f1f644070ff359d3803dff5264c5afcc70a09
Cristian Rubio <cristianfrubio01@gmail.com>
2024-07-26 19:41:04 -0500

    Se modifica el componente de valores para dejarlos todo en linea y tambien su parte responsive

--- src/components/Values/index.jsx
+++ src/components/Values/index.jsx
@@ -4,54 +4,30 @@ import igualdad from "../../assets/inicio/values/valor-igualdad.png";
 import confidencialidad from "../../assets/inicio/values/valores-confidencialidad.png";
 import imparcialidad from "../../assets/inicio/values/valor-imparcialidad.png";
 
+const datos = [
+  { title: "Igualdad", img: igualdad, description: "Tratamiento equitativo hacia las personas que solicitan y acceden al proceso de acuerdo a los lineamientos de esquema de certificación."},
+  { title: "Confidencialidad", img: confidencialidad, description: "Garantía de que la información personal será protegida para que no sea divulgada sin consentimiento de la persona que accede a los servicios."},
+  { title: "Imparcialidad", img: imparcialidad, description: "Presencia de objetividad en los procesos de certificación."},
+];
+
 const Values = () => {
   return (
     <div className="values mt-4">
       <div className="values__container">
-
         <div className="values__title mb-4">
           <h2 className="text-center">NUESTROS VALORES</h2>
         </div>
-
-        <div className="values__content row g-3">
-            
-          <div className="valie__container col-md">
-            <figure>
-                <img src={igualdad} alt="igualdad"/>
+        <div className="values__content mt-4 row g-3">
+          {datos.map((dato, index) => (
+            <div className="valie__container col-md-4" key={index}>
+              <figure>
+                <img src={dato.img} alt={dato.title} />
                 <div className="valie_info">
-                    <p>
-                      Tratamiento equitativo hacia las personas que solicitan
-                      y acceden al proceso de acuerdo a los lineamientos de 
-                      esquema de certificación.
-                    </p>
+                  <p>{dato.description}</p>
                 </div>
-            </figure>                        
-          </div>
-
-          <div className="valie__container col-md">
-            <figure>
-                <img src={confidencialidad} alt="confidencialidad"/>
-                <div className="valie_info">
-                    <p>
-                      Garantía de que la información personal será protegida para
-                      que no sea divulgada sin consentimiento de la persona que 
-                      accede a los servicios.
-                    </p>
-                </div>
-            </figure>                        
-          </div>
-
-          <div className="valie__container col-md">
-            <figure>
-                <img src={imparcialidad} alt="imparcialidad"/>
-                <div className="valie_info">
-                    <p>
-                      Presencia de objetividad en los procesos de certificación.
-                    </p>
-                </div>
-            </figure>
-          </div>
-
+              </figure>
+            </div>
+          ))}
         </div>
       </div>
     </div>
