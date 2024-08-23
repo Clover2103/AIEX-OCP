a75246cbb16b700dcb92584385ce74e50ebcc0b0
Cristian Rubio <cristianfrubio01@gmail.com>
2024-08-23 16:04:43 -0500

    Se modifica el componente de registro para para para centralizar el hook con el mismo componente por funcionalidad ajax

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { ContactForm } from "../../components/ContactForm";
 import { ScopesSliderStatic } from '../../components/ScopesSliderStatic';
-
+import image from "../../assets/contacto/contacto.png"
 
 const Contact = () => {
   return (
     <div>
       <ScopesSliderStatic
-        // imageSrc={image}
+        imageSrc={image}
         title="CONTACTO"
       />
       <div className="container">
